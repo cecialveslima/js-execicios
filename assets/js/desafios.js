4ff32798f368fe8c44fc0270df49a5729fb1213c
cecialveslima <cecialveslima@gmail.com>
2022-07-08 10:21:15 -0300

    Desafios fase 1

---
+++ assets/js/desafios.js
@@ -0,0 +1,37 @@
+/*
+var aux = 1;
+var N = 6;
+var resultado = 2;
+for (let i = 2; i <= N; i++) {
+
+	if (i % 2 ===  N  ) 
+          print(`${i}^2 = ${   i * i   }`);
+          console.log(`${i}^2 = ${ (i ** 2 )  }`);
+          i += 1 ;
+}
+*/
+/*
+var N = 3;
+let x ;
+
+for ( let i = 1; i < 50; i++) {
+         x = i% N;
+        if (x==2) console.log(i);
+   }
+
+*/
+
+/*
+//let lines = gets().split("\n");
+//let line = lines.shift().split(' ');
+let soma = 0
+
+//TODO: Complete os espaços em branco com uma possível solução para o desafio
+//let array = line.split(' ');
+
+for(i=0;i<  line.length   ;i++){
+  let transfor = parseInt(line[ i  ])
+  soma = soma + transfor
+}
+print(soma-3)
+*/
